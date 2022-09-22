7aab9f67ad0c0f096d1d4a465fdf72825416a2a1
b002557 <aaron_lin@mail.bok.com.tw>
2022-09-22 17:17:03 +0800

    Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,29 @@
-$( document ).ready(function() {
-  console.log('Hello World');
+$(function() {
+  $('.slideImageWrapper').slick();
+
+  // click language option
+  $('.langOption').on('click', function(e) {
+    let locale = $(this).attr('value');
+    
+    $.i18n({
+      locale: locale
+    });
+    $('body').i18n();
+  });
+
+  // 預設英文
+  $.i18n({
+    locale: 'en_US'
+  });
+
+  // 讀取語系檔
+  $.i18n().load({
+    'en_US': './i18n/en_US.json',
+    'zh_TW': './i18n/zh_TW.json',
+    'zh_CN': './i18n/zh_CN.json'
+  }).done(function (){
+
+    // 語系初始化
+    $('body').i18n();
+  });
 });
