f2b844fad872d66a154145ad5bc140102d2ee7d1
penny wang <pennywang@mail.bok.com.tw>
2022-09-22 17:01:40 +0800

    index layout

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
   console.log('Hello World');
+
+  $('.slideImageWrapper').slick();
+
+  // click language option
+  $('.langOption').on('click', function(e) {
+    console.log('Hello Worldfewf');
+    let locale = $(this).attr('val');
+    alert(locale);
+  });
 });
+
