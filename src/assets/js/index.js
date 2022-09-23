e9d4bdabcbb4f35e991ed4ad4f9fa93960197f87
b002557 <aaron_lin@mail.bok.com.tw>
2022-09-23 14:48:31 +0800

    加入 go to top, i18n 判斷

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,10 @@
 $(function() {
+  // i18n 初始化
+  i18nInit();
+
+  // Go to top 初始化
+  goToTopInit();
+
   $('.slideImageWrapper').slick({
     dots: false,
     infinite: true,
@@ -10,21 +16,26 @@ $(function() {
     autoplaySpeed: 2000,
   });
 
-  // click language option
-  $('.langOption').on('click', function(e) {
-    let locale = $(this).attr('value');
-    $('.langOption').removeClass('active');
-    $(this).addClass('active');
-    
-    $.i18n({
-      locale: locale
-    });
-    $('body').i18n();
-  });
+  
+  // draggable bk
+  $('div.draggableBK').backgroundDraggable();
+});
 
+/**
+ * i18nInit
+ * i18n 初始化
+ * 
+ */
+function i18nInit() {
+  var lang = localStorage.getItem(I18N_LANG_KEY);
+  
   // 預設英文
+  if (lang === undefined || lang === null) {
+    lang = 'en_US';
+  }
+
   $.i18n({
-    locale: 'en_US'
+    locale: lang,
   });
 
   // 讀取語系檔
@@ -37,7 +48,40 @@ $(function() {
     // 語系初始化
     $('body').i18n();
   });
+  
+  // click language option
+  $('.langOption').on('click', function(e) {
+    let locale = $(this).attr('value');
+    $('.langOption').removeClass('active');
+    $(this).addClass('active');
+    
+    localStorage.setItem(I18N_LANG_KEY, locale);
 
-  // draggable bk
-  $('div.draggableBK').backgroundDraggable();
-});
+    $.i18n({
+      locale: locale
+    });
+    $('body').i18n();
+  });
+}
+
+/**
+ * goToTopInit
+ * Go to top 初始化
+ * 
+ */
+function goToTopInit() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+}
