e43ea83ea53bd1d9e77a1b52f8bea3025648f226
b002557 <aaron_lin@mail.bok.com.tw>
2022-10-11 17:57:43 +0800

    Updates

--- gamelive/gamelive.js
+++ gamelive/gamelive.js
@@ -1,3 +1,28 @@
+// 追號
+var ADD_SEETING_DATA = null;
+
 $(function() {
-  console.log('Page Init');
+  mobileSelectInit();
 });
+
+/**
+ * mobileSelectInit
+ * 
+ * 
+ */
+function mobileSelectInit() {
+  const buttonArr = [ '1', '2', '3', '4', '5', '10', '15', '20', '25', '30' ];
+
+  new MobileSelect({
+    trigger: document.querySelector("#add_setting_select"),
+    wheels: [{ data: buttonArr }],
+    initValue: ADD_SEETING_DATA,
+    ensureBtnText: '確認',
+    cancelBtnText: '取消',
+    onTransitionEnd: function (data, indexArr, msInstance) {
+    },
+    onChange: function (data, indexArr, msInstance) {
+      console.log(data);
+    },
+  });
+}
