80ed859456e5762de3541c4229be2d7d9ba0ddc7
b002557 <aaron_lin@mail.bok.com.tw>
2022-10-14 14:36:55 +0800

    Updates

--- gamelive/gamelive.js
+++ gamelive/gamelive.js
@@ -4,6 +4,8 @@ var ADD_SEETING_DATA = null;
 $(function() {
   mobileSelectInit();
 
+  getData();
+
   initCountDown();
 });
 
@@ -27,6 +29,68 @@ function mobileSelectInit() {
       console.log(data);
     },
   });
+}
+
+/**
+ * getData
+ * 模擬取得後端假資料
+ * 
+ */
+function getData() {
+  const data = [
+    {
+      number: '0000000001',
+      unit: '111',
+    },
+    {
+      number: '0000000002',
+      unit: '222',
+    },
+    {
+      number: '0000000003',
+      unit: '333',
+    },
+    {
+      number: '0000000004',
+      unit: '444',
+    },
+    {
+      number: '0000000005',
+      unit: '555',
+    },
+    {
+      number: '0000000006',
+      unit: '666',
+    },
+    {
+      number: '0000000007',
+      unit: '777',
+    },
+    {
+      number: '0000000008',
+      unit: '888',
+    },
+  ];
+
+  var tmpHtml = '';
+
+  for (var i = 0; i < data.length; i++) {
+    tmpHtml = tmpHtml + '<div class="table_row">';
+    tmpHtml = tmpHtml + '  <div class="column_number table_row_color">';
+    tmpHtml = tmpHtml + data[i].number;
+    tmpHtml = tmpHtml + '  </div>';
+    tmpHtml = tmpHtml + '  <div class="column_time table_row_color"><span name="count_down">--</span></div>';
+    tmpHtml = tmpHtml + '  <div class="column_unit table_row_color">';
+    tmpHtml = tmpHtml + data[i].unit;
+    tmpHtml = tmpHtml + '  </div>';
+    tmpHtml = tmpHtml + '  <div class="column_action table_row_color">';
+    tmpHtml = tmpHtml + '    <button class="cancel_button">取消</button>';
+    tmpHtml = tmpHtml + '  </div>';
+    tmpHtml = tmpHtml + '</div>';
+
+  }
+
+  $('#data_row').html(tmpHtml);
 }
 
 /**
