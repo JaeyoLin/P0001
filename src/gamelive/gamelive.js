7fb5dfd4fd46bd4590c786d1b1def8787a957b6a
b002557 <aaron_lin@mail.bok.com.tw>
2022-10-14 14:20:30 +0800

    11

--- src/gamelive/gamelive.js
+++ src/gamelive/gamelive.js
@@ -3,6 +3,8 @@ var ADD_SEETING_DATA = null;
 
 $(function() {
   mobileSelectInit();
+
+  initCountDown();
 });
 
 /**
@@ -19,10 +21,29 @@ function mobileSelectInit() {
     initValue: ADD_SEETING_DATA,
     ensureBtnText: '確認',
     cancelBtnText: '取消',
-    onTransitionEnd: function (data, indexArr, msInstance) {
-    },
+    // onTransitionEnd: function (data, indexArr, msInstance) {
+    // },
     onChange: function (data, indexArr, msInstance) {
       console.log(data);
     },
   });
+}
+
+/**
+ * initCountDown
+ * 
+ */
+function initCountDown() {
+  // 倒數三分鐘 60 X 3
+  var countDown = 180;
+  var timer = $("[name='count_down']");
+  
+  setInterval(function() {
+    
+  countDown = countDown - 1;
+    if (countDown <= 0 ) {
+      countDown = 0;
+    }
+    timer.text(countDown);
+  }, 1000);  
 }
